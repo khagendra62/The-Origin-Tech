a513a891dbb75f0d74b1474305347f8a6c7e7f47
khagendra62 <khagendraawasthi47@gmail.com>
2025-08-24 02:43:39 +0545

    image at aboutus

--- src/pages/AboutUs/TeamImages/TeamImages.jsx
+++ src/pages/AboutUs/TeamImages/TeamImages.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import "./TeamImages.css";
+import award1 from "../../../assets/awards/award1.jpeg";
+import award2 from "../../../assets/awards/award2.jpeg";
+import logo from "../../../assets/awards/logo.png";
 
 const TeamImages = () => {
   return (
     <div className="TeamImageContainer">
-      <div className="ourWebTeam">Our Tech Team</div>
+      {/* <div className="ourWebTeam">Our Tech Team</div> */}
+      <div className="ourWebTeam">Our Awards</div>
       <div className="TeamImageContainer-track">
         <a
-          href="https://mcquaig.co.uk/wp-content/uploads/2013/11/Happy-team.jpg"
+          href={logo}
           target="_blank"
           rel="noopener noreferrer"
           className="card imageCard1"
         >
-          <img
-            src="https://mcquaig.co.uk/wp-content/uploads/2013/11/Happy-team.jpg"
-            alt="Team Photo 1"
-          />
+          <img src={logo} alt="Team Photo 1" />
           <div className="TeamImageContainerInfo">
             <h3>John Smith</h3>
             <p>Project Manager</p>
@@ -23,15 +24,12 @@ const TeamImages = () => {
         </a>
 
         <a
-          href="https://images.unsplash.com/photo-1690378820474-b468b8ee64d3?fm=jpg&q=60&w=3000"
+          href={award2}
           target="_blank"
           rel="noopener noreferrer"
           className="card imageCard2"
         >
-          <img
-            src="https://images.unsplash.com/photo-1690378820474-b468b8ee64d3?fm=jpg&q=60&w=3000"
-            alt="Team Photo 2"
-          />
+          <img src={award2} alt="Team Photo 2" />
           <div className="TeamImageContainerInfo">
             <h3>Emily Johnson</h3>
             <p>UI/UX Designer</p>
@@ -39,15 +37,12 @@ const TeamImages = () => {
         </a>
 
         <a
-          href="https://plus.unsplash.com/premium_photo-1661767467261-4a4bed92a507?fm=jpg&q=60&w=3000"
+          href={award1}
           target="_blank"
           rel="noopener noreferrer"
           className="card imageCard3"
         >
-          <img
-            src="https://plus.unsplash.com/premium_photo-1661767467261-4a4bed92a507?fm=jpg&q=60&w=3000"
-            alt="Team Photo 3"
-          />
+          <img src={award1} alt="Team Photo 3" />
           <div className="TeamImageContainerInfo">
             <h3>Michael Lee</h3>
             <p>Software Engineer</p>
@@ -55,15 +50,12 @@ const TeamImages = () => {
         </a>
 
         <a
-          href="https://mcquaig.co.uk/wp-content/uploads/2013/11/Happy-team.jpg"
+          href={award2}
           target="_blank"
           rel="noopener noreferrer"
           className="card imageCard6"
         >
-          <img
-            src="https://mcquaig.co.uk/wp-content/uploads/2013/11/Happy-team.jpg"
-            alt="Team Photo 4"
-          />
+          <img src={award2} alt="Team Photo 4" />
           <div className="TeamImageContainerInfo">
             <h3>John Smith</h3>
             <p>Project Manager</p>
@@ -71,15 +63,12 @@ const TeamImages = () => {
         </a>
 
         <a
-          href="https://images.unsplash.com/photo-1690378820474-b468b8ee64d3?fm=jpg&q=60&w=3000"
+          href={logo}
           target="_blank"
           rel="noopener noreferrer"
           className="card imageCard7"
         >
-          <img
-            src="https://images.unsplash.com/photo-1690378820474-b468b8ee64d3?fm=jpg&q=60&w=3000"
-            alt="Team Photo 2"
-          />
+          <img src={logo} alt="Team Photo 2" />
           <div className="TeamImageContainerInfo">
             <h3>Emily Johnson</h3>
             <p>UI/UX Designer</p>
